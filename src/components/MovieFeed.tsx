5d11029d40c30f462390a704ff32c1c77a851a68
LouisThmas <louisssthomasss@gmail.com>
2025-01-06 16:24:02 -0500

    Saving function completed with the change in icon

--- src/components/MovieFeed.tsx
+++ src/components/MovieFeed.tsx
@@ -2,6 +2,7 @@ import "./MovieFeed.css";
 import { useEffect, useState } from "react";
 import getMatchingMovies from "../utils/MovieFetcher";
 import saveMovie from "../utils/MovieSaver";
+import SavedFetcher from "../utils/SavedFetcher";
 
 type MovieFeedProps = {
     isSent: boolean;
@@ -21,6 +22,8 @@ type Movie = {
 function MovieFeed ({ isSent, thought, movies }: MovieFeedProps) {
     const [movieList, setMovieList] = useState<Movie[]>(movies);
     const [pageNumber, setPageNumber] = useState<number>(1);
+    // Stores the ids of the movies that are saved
+    const [savedMovies, setSavedMovies] = useState<number[]>([0])
 
     async function loadMoreMovies() {
         const newMovies = await getMatchingMovies(thought, pageNumber);
@@ -28,13 +31,32 @@ function MovieFeed ({ isSent, thought, movies }: MovieFeedProps) {
         setPageNumber((prevPage) => prevPage + 1);
     }
 
+    // Asynchronous function to save movies
+    // Also adds the saved movie to the save list
     async function save(id: number, title: string, runtime: string, year: string) {
-        const savedMovie = saveMovie(id, title, runtime, year);
+        saveMovie(id, title, runtime, year);
+        setSavedMovies((savedMovies) => [...savedMovies, id])
     }
 
     // Set the movie list when the prompt is sent (to not have an empty list first)
+    // Get the movies that are saved by the user aswell
     useEffect(() => {
         setMovieList(movies);
+
+        async function fetchSavedMovies() {
+            try {
+                const movies = await SavedFetcher();
+
+                movies.forEach(element => {
+                    setSavedMovies((movies) => [...movies, element.movieId])
+                    console.log(element.movieId)
+                });
+            } catch (error) {
+                console.error("Error fetching saved movies:", error);
+            }
+        }
+        fetchSavedMovies();
+        console.log("here are the saved movies",savedMovies)
     }, [isSent])
 
     useEffect(() => {
@@ -80,7 +102,7 @@ function MovieFeed ({ isSent, thought, movies }: MovieFeedProps) {
                         <p>{movie.rating * 10}★</p>
                     </div>
                     <p>{movie.description}</p>
-                    <button onClick={() => save(movie.id, movie.title, movie.runtime, movie.release_year)}><img className="save-icon" src="src/assets/save.png"/></button>
+                    <button className="save-icon" onClick={() => save(movie.id, movie.title, movie.runtime, movie.release_year)}><img className={`save-icon ${savedMovies.includes(movie.id)}`}/></button>
                     <hr></hr>
                 </div>
             ))}
